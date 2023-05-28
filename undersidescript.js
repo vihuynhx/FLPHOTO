cc9d666ea8d30c6c40665f62acd846c93fd796b6
kath1666 <katharinaskifter@hotmail.com>
2023-05-28 18:26:01 +0200

    Update

--- undersidescript.js
+++ undersidescript.js
@@ -30,3 +30,24 @@ document.addEventListener('DOMContentLoaded', function() {
     carouselItems.style.transform = 'translateY(' + translateYValue + ')';
   }
 });
+
+
+
+//SEND KNAP ALERT
+function validerfelter()
+{
+	let navn = document.getElementById("skrivBoks").value; 
+	let email = document.getElementById("skrivBoks").value;
+	let besked = document.getElementById("beskedIndhold").value;
+	let kategori = document.getElementById("selectKategori").value;
+
+	if(navn  == "" || email  == "" || besked  == ""|| kategori  == "" ) //hvis navn,mail,besked,kategori = intet input
+	{
+		alert("Udfyld venligst alle felter");						//opstår alert
+	}
+	else
+	{
+		window.location.href = "tak.html";							//ellers kommer man ind på tak.html
+	}
+
+}
