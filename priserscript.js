4ef7c1eecf974877a55d55c5d468201f970d9881
YasminSofia <114469027+YasminSofia@users.noreply.github.com>
2023-05-29 14:56:18 +0200

    update

--- priserscript.js
+++ priserscript.js
@@ -1,6 +1,6 @@
 // Mobil header 
-var burgerMenu = document.getElementById('burger-menu');
-var overlay = document.getElementById('menu');
+let burgerMenu = document.getElementById('burger-menu');
+let overlay = document.getElementById('menu');
 burgerMenu.addEventListener('click',function(){
   this.classList.toggle("close");
   overlay.classList.toggle("overlay");
