866e9539fb83d9d155f8f1e2a7f3692065520c96
vihuynhx <eaamvth@students.eaaa.dk>
2023-05-29 10:59:54 +0200

    update

--- undersidescript.js
+++ undersidescript.js
@@ -1,33 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let carouselContainer = document.querySelector('.carousel-container');
-  let carouselItems = document.querySelector('.carousel-items');
-  let carouselUp = document.querySelector('.carousel-up');
-  let carouselDown = document.querySelector('.carousel-down');
-  let itemHeight = carouselContainer.offsetHeight / 3; // Opdateret højde for 3 synlige billeder
+document.addEventListener('DOMContentLoaded', function() 
+{
+  let karuselContainer = document.querySelector('.karuselContainer');
+  let karuselBilleder = document.querySelector('.karuselBilleder');
+  let karuselOp = document.querySelector('.karuselOp');
+  let karuselNed = document.querySelector('.karuselNed');
+  let karuselhojde = karuselContainer.offsetHeight / 3; // Opdateret højde for 3 synlige billeder
   let currentIndex = 0;
 
 
-  carouselItems.style.transform = 'translateY(0)'; // Startposition for de synlige billeder
+  karuselBilleder.style.transform = 'translateY(0)'; // Startposition for de synlige billeder
 
-  carouselUp.addEventListener('click', function() {
+  karuselOp.addEventListener('click', function()
+  {
     currentIndex--;
     if (currentIndex < 0) {
-      currentIndex = carouselItems.childElementCount - 3; // Opdateret grænseværdi
+      currentIndex = karuselBilleder.childElementCount - 3; // Opdateret grænseværdi
     }
     updateCarouselPosition();
   });
 
-  carouselDown.addEventListener('click', function() {
+  karuselNed.addEventListener('click', function()
+  {
     currentIndex++;
-    if (currentIndex >= carouselItems.childElementCount) {
+    if (currentIndex >= karuselBilleder.childElementCount) {
       currentIndex = 0;
     }
     updateCarouselPosition();
   });
 
-  function updateCarouselPosition() {
-    let translateYValue = -currentIndex * itemHeight + 'px';
-    carouselItems.style.transform = 'translateY(' + translateYValue + ')';
+  function updateCarouselPosition()
+  {
+    let translateYValue = -currentIndex * karuselhojde + 'px';
+    karuselBilleder.style.transform = 'translateY(' + translateYValue + ')';
   }
 });
 
