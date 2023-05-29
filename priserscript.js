f1ef1167d97a405116047b6f41003f871218c983
vihuynhx <eaamvth@students.eaaa.dk>
2023-05-30 00:12:36 +0200

    update

--- priserscript.js
+++ priserscript.js
@@ -1,39 +1,50 @@
 // Mobil header 
+
+//henter variabler fra html
 let burgerMenu = document.getElementById('burger-menu');
 let overlay = document.getElementById('menu');
-burgerMenu.addEventListener('click',function(){
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
+
+burgerMenu.addEventListener('click',function() //laver en click-listener for burgermenu
+{
+  this.classList.toggle("close"); //toggle tilføjer eller fjerner "close" til elementet. Hvis "close" eksisterer bliver den fjernet, hvis den ikke eksisterer bliver den tilføjet.
+  overlay.classList.toggle("overlay"); //samme sker for overlay
 });
 
 
-// Slideshow
+// Slideshow https://www.w3schools.com/w3css/w3css_slideshow.asp
+let slideIndex = 1; //opretter slideIndex varibel med sætter lig med 1 som start
+showSlides(slideIndex); // kalder funktionen så den første slide, billede, vises
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n) //laver en funktion til argument n
+{
+  showSlides(slideIndex += n); //kalder slideshow + med den opdateret index
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n) //laver en funktion med den nuværende slide
+{
+  showSlides(slideIndex = n); //kalder og viser og nuværende slide
 }
 
-function showSlides(n) {
+function showSlides(n) 
+ { //henter elementer fra HTML og gemmer i myslides og dor
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+
+  if (n > slides.length) {slideIndex = 1}    //hvis værdien af slide-længden er større end antallet af slides = 1. Dvs, når man når sidste slide vil man se det første igen ved næste 
+  if (n < 1) {slideIndex = slides.length} //hvis værdien af slide-længden er mindre end antallet af slides, vil man se det sidste slide, når man går tilbage fra det første
+
+  for (i = 0; i < slides.length; i++) //laver en for løkke som gennengår alle slides 
+  {
+    slides[i].style.display = "none";  //viser display none for andre slides end den nuværende slide
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+//opdatere visningen af slides og markører i slideshowet
+  for (i = 0; i < dots.length; i++) 
+  {
+    dots[i].className = dots[i].className.replace(" active", ""); //replacer active med "" - det fjerner effekten af at aktivt dot og gør det inaktivt.
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].style.display = "block"; //synliggøre slide ved at gøre det til block
+  dots[slideIndex-1].className += " active"; //gør den aktuelle slide actice
 }
 
 
--- undersidescript.js
+++ undersidescript.js
@@ -1,37 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() 
-{
+
+document.addEventListener('DOMContentLoaded', function() //DOMContentLoaded er en event som sikre at hele kode-dokumentet (DOM) er læst og analyseret igennem og er klar til at blive manipuleret med vha.js 
+{ //opretter og gemmer variabler fra HTML
   let karuselContainer = document.querySelector('.karuselContainer');
   let karuselBilleder = document.querySelector('.karuselBilleder');
   let karuselOp = document.querySelector('.karuselOp');
   let karuselNed = document.querySelector('.karuselNed');
-  let karuselhojde = karuselContainer.offsetHeight / 3; // Opdateret højde for 3 synlige billeder
-  let currentIndex = 0;
+  let karuselhojde = karuselContainer.offsetHeight / 3; //offsetHeight returnere højden på karuselContainer hvor jeg divider med 3, for at vise 3 synlige billeder 
+  let vistPaSkaerm = 0; //lyder det som bliver vist på skærmen til at være lig med 0 for at holde styr på hvad der vises i karusellen
 
 
-  karuselBilleder.style.transform = 'translateY(0)'; // Startposition for de synlige billeder
+  karuselBilleder.style.transform = 'translateY(0)'; //translateY(0) er en css funktion som sætter y-aksen til være 0, så karuselBilleder ikke flytter sig lodret men forbliver på startspositionen. 
 
-  karuselOp.addEventListener('click', function()
+  karuselOp.addEventListener('click', function() //opretter en click-handler til op-knappen
   {
-    currentIndex--;
-    if (currentIndex < 0) {
-      currentIndex = karuselBilleder.childElementCount - 3; // Opdateret grænseværdi
-    }
-    updateCarouselPosition();
+    vistPaSkaerm--; //Gør vistPaSkaerem mindre med -1 så det "viser" det forrige billede i karusellet 
+    if (vistPaSkaerm < 0) //hvis den er mindre en 0 så er karusellet på det første billede 
+      {
+        vistPaSkaerm = karuselBilleder.childElementCount - 3; // så returnerer childElementCountantallet af karuselBilleder med -3 som betyder at der bliver trukket 3 billeder af antal synlige på skærmen 
+      }                                                       // altså den går 3 tilbage fra det første billede, når man er på det første billede og trykker op.
+    updateCarouselPosition(); //kalder den opdateret karusel med dens nuværende position, altså de 3 billeder
   });
 
-  karuselNed.addEventListener('click', function()
+  karuselNed.addEventListener('click', function() //opretter en click-handler til ned-knappen
   {
-    currentIndex++;
-    if (currentIndex >= karuselBilleder.childElementCount) {
-      currentIndex = 0;
-    }
-    updateCarouselPosition();
+    vistPaSkaerm++; //øger skærmen med +1 så den viser det næste billede
+    if (vistPaSkaerm >= karuselBilleder.childElementCount) 
+      { //tjekker om skærmen er større eller lig med antallet med antal child
+        vistPaSkaerm = 0; //så bliver skærmen nulstillet med 0 for at starte karusellen forfra
+      }
+    updateCarouselPosition(); //kalder og opdatere
   });
 
-  function updateCarouselPosition()
-  {
-    let translateYValue = -currentIndex * karuselhojde + 'px';
-    karuselBilleder.style.transform = 'translateY(' + translateYValue + ')';
+  function updateCarouselPosition() //en funktion som updatere karusellens postion (vertikalt i y-aksen)
+  { // laver en beregning på y-aksen-value, for at finde ud af hvor meget karusellen skal flyttes op eller ned.  
+    let translateYValue = -vistPaSkaerm * karuselhojde + 'px'; //ved at gange antallet af synlige billeder vi ønsker med højden på hvert billede får vi en y-akse-value som vi sætter minus foran for at ændre retningen (nedad), da vi ellers får en positivt tal som vil bevæge sig opad)
+    karuselBilleder.style.transform = 'translateY(' + translateYValue + ')'; // nu ændre vi style på karuselbilleder med y for at transform den til den beregenede y-akse-value så den kan flytte sig opad og nedad
   }
 });
 
